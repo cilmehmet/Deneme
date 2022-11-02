191fd1917b38df6c9e343db7fb0a23bb95fb8eb0
Mehmetcil <mehmetcil096@gmail.com>
2022-11-03 01:52:17 +0300

    Deneme

---
+++ src/A01.java
@@ -0,0 +1,9 @@
+public class A01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello github");
+
+
+    }
+}
---
+++ src/B01.java
@@ -0,0 +1,6 @@
+public class B01 {
+
+    public static void main(String[] args) {
+        String str = "ali";
+    }
+}
